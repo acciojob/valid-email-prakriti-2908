1a640844933d9bb815439ad3be0298248855c768
prakriti-2908 <77171197+prakriti-2908@users.noreply.github.com>
2024-02-15 21:38:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function validEmail(str) {
   //your JS code here.
+	if((str.includes(".com")||str.includes(".in")||str.includes(".edu")||str.includes(".co")||str.includes(".in"))&& str.includes("@")&&str.charAt(0)!="@"){
+      return true;
+  }
+  return false;
 }
 
 // Do not change the code below.
