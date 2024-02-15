8f6e050d93fd21bfdb4767cdf852cd9aa4afc06d
prakriti-2908 <77171197+prakriti-2908@users.noreply.github.com>
2024-02-15 21:44:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 function validEmail(str) {
   //your JS code here.
-	if((str.includes(".com")||str.includes(".in")||str.includes(".edu")||str.includes(".co")||str.includes(".in"))&& str.includes("@")&&str.charAt(0)!="@"){
-      return true;
-  }
-  return false;
+	// if((str.includes(".com")||str.includes(".in")||str.includes(".edu")||str.includes(".co")||str.includes(".in"))&& str.includes("@")&&str.charAt(0)!="@"){
+ //      return true;
+ //  }
+ //  return false;
+
+	if(str[0]=="@"){
+    return false;
+}
+let ans1 = false;
+let ans2 = false;
+
+for(let i=0;i<str.length;i++){
+    if(ans1==true && ans2==true){
+        return true;
+    }
+    if(ans1==false && (str[i]==".com" || str[i]==".in" || str[i]==".co" || str[i]==".edu")){
+        ans1 = true;
+    }
+    if(ans2==false && str[i]=="@"){
+        ans2 = true;
+    }
+}
+if(ans1==true && ans2==true){
+        return true;
+    }else{
+        return false;
+    }
 }
 
 // Do not change the code below.
